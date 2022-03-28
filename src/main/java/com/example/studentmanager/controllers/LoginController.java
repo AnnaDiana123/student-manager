1132ba67195b80bc560af9b3324cb60264e182d0
AnnaDiana123 <101590349+AnnaDiana123@users.noreply.github.com>
2022-03-28 15:25:47 +0300

    Update LoginController.java

--- src/main/java/com/example/studentmanager/controllers/LoginController.java
+++ src/main/java/com/example/studentmanager/controllers/LoginController.java
@@ -28,15 +28,21 @@ public class LoginController {
             return;
         }
 
-        if (username.equals("student") && password.equals("student")) {
-            loginMessage.setText("Logged in as student!");
-            return;
-        }
-
         if (username.equals("teacher") && password.equals("teacher")) {
-            loginMessage.setText("Logged in as teacher!");
-            return;
-        }
+   try {
+       Stage stage = (Stage) loginMessage.getScene().getWindow();
+       Parent viewStudentsRoot = FXMLLoader.load(getClass().getResource("../fxml/view-students.fxml"));
+       Scene scene = new Scene(viewStudentsRoot, 600, 400);
+       stage.setScene(scene);
+   } catch (IOException e) {
+       e.printStackTrace();
+   }
+
+   return;
+}
+
+
+       
 
         loginMessage.setText("Incorrect login!");
     }
